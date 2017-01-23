88ddc3d1394209506100428d43ea5b3df0fc9a0a
Flavio Mascetti <masciospam@openmailbox.org>
2017-01-23 17:43:38 +0100

    [ADD] conversation handling

--- default_messages.php
+++ default_messages.php
@@ -1,5 +1,6 @@
 <?php
 define('START_MSG', 'Come posso aiutarti?');
-define('VALUTAZIONE_MSG', "Vuoi darmi un voto? \n Inserisci un numero compreso tra 1 e 5 :)");
+define('VALUTAZIONE_MSG_0', "Vuoi darmi un voto? \n Inserisci un numero compreso tra 1 e 5 :)");
+define('VALUTAZIONE_MSG_1', "Grazie per avermi valutato!");
 define('ERROR_UNKNOWN_COMMAND', 'Mi dispiace, non conosco il comando da te inserito');
 ?>
--- msg_processing_simple.php
+++ msg_processing_simple.php
@@ -1,4 +1,41 @@
 <?php
+
+// Function definitions
+
+function handle_conversation($chat_id, $from_id, $message, $conv) {
+    if($conv === false) {
+        // Query failed
+        return false;
+    }
+    if($conv == null) {
+        // No existing conversation with the user
+        return false;
+    }
+
+    $topic = $conv[1];
+    $state = $conv[2];
+    $text = $message['text'];
+
+    switch($state) {
+        case 1:
+	    $vote = intval($text);
+	    if ($vote >= 1 && $vote <= 5){
+		db_perform_action ("REPLACE INTO bot_votes VALUES($from_id, $vote)");
+            	telegram_send_message($chat_id, VALUTAZIONE_MSG_1);
+	    }
+	    else {
+		// Handle wrong vote
+	    }
+
+            db_perform_action("DELETE FROM `conversation` WHERE `user_id` = $from_id");
+            return true;
+    }
+
+    return false;
+}
+
+// End of Function declarations
+
 $message_id = $message['message_id'];
 $chat_id = $message['chat']['id'];
 $from_id = $message['from']['id'];
@@ -6,16 +43,30 @@ $from_id = $message['from']['id'];
 if (isset($message['text'])) {
     // Got an incoming text message
     $text = $message['text'];
+    $conv = db_row_query("SELECT `user_id`, `topic`, `state` FROM `conversation` WHERE `user_id` = $from_id");
+    $handled_conv = true;
 
-    if (strpos($text, "/") === 0) {
+    if ($conv != null)
+	$handled_conv = handle_conversation($chat_id, $from_id, $message, $conv);
+
+    else if (strpos($text, "/") === 0) {
 	// Received a command
 	switch (substr($message['text'], 1)){
 		case 'start':{
 			telegram_send_message($chat_id, 'Ciao ' . $message['from']['first_name'] . '!' . "\n" . START_MSG);
+			break;
 		}
 		case 'valutabot':{
+        		db_perform_action("REPLACE INTO `conversation` VALUES($from_id, 'valutazione', 1)");
+
+       			telegram_send_message($chat_id, VALUTAZIONE_MSG_0);
+			break;
 		}
 		case 'rating':{
+		}
+		default:{
+			telegram_send_message($chat_id, ERROR_UNKNOWN_COMMAND);
+			break;
 		}
 	}
     }
@@ -44,6 +95,9 @@ if (isset($message['text'])) {
 	$bot_response = $json_response['botsay'];
 	telegram_send_message($chat_id, $bot_response);
     }
+
+    if ($handled_conv != true)
+    	db_perform_action("DELETE FROM `conversation` WHERE `user_id` = $from_id");
 }
 else {
     telegram_send_message($chat_id, 'Sorry, I understand only text messages at the moment!');
