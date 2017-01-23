78e0cb4912b4c02432fbbc98cf4c033c5b659f5f
Flavio Mascetti <masciospam@openmailbox.org>
2017-01-23 16:44:45 +0100

    [MOD] default messages

--- default_messages.php
+++ default_messages.php
@@ -1,3 +1,5 @@
 <?php
 define('START_MSG', 'Come posso aiutarti?');
+define('VALUTAZIONE_MSG', "Vuoi darmi un voto? \n Inserisci un numero compreso tra 1 e 5 :)");
+define('ERROR_UNKNOWN_COMMAND', 'Mi dispiace, non conosco il comando da te inserito');
 ?>
