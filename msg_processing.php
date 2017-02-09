49f63694d823b942bc2f3ad3683dede63b25fc8e
Flavio Mascetti <masciospam@openmailbox.org>
2017-02-09 22:38:19 +0100

    [FIX] string matching problem

--- msg_processing.php
+++ msg_processing.php
@@ -53,7 +53,7 @@ if (isset($message['text']) && $handled_conv == false) {
 	$bot_response = $json_response['botsay'];
 
 	if (strpos($bot_response, "/") === 0) {
-		// Received a command
+		// Received a command from the bot
 		command_handle ($chat_id, $from_id, $message, $bot_response);
         }
 	else{
@@ -74,7 +74,7 @@ else {
 function command_handle ($chat_id, $from_id, $message, $text){
     switch (substr($text, 1)){
 		case 'start':{
-			$keyboard = prepare_button_array (array(array('Lista Biblioteche', 'Valuta bot'), array('Rating bot', 'Segnala aula studio'), array('Trova biblioteca/aula studio più vicina alla tua posizione')));
+			$keyboard = prepare_button_array (array(array('Lista Biblioteche', 'Valuta bot'), array('Rating bot', 'Segnala aula studio'), array("Vicino alla tua posizione")));
 			telegram_send_message($chat_id, replace_placeholder (START_MSG, $message), $keyboard);
 			break;
 		}
